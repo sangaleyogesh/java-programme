1f39bf46ab0f7a94cddbe87f0979b87617c7ca92
Yogesh Sangale <Yogesh Sangale@LAPTOP-ICF8JRD8>
2022-12-08 21:59:55 +0530

    ascending array prog added

---
+++ programme sequence JAVA/src/array/arrayascendingorder.java
@@ -0,0 +1,33 @@
+package array;
+
+public class arrayascendingorder {
+	
+	public static void main(String[]args) {
+
+	
+	int arr[]= {10,5,6,8,25,2};
+	
+	int temp;
+	
+	
+	for(int i=0; i<arr.length; i++) {
+		
+		for(int j=i; j<arr.length; j++) {
+			
+			
+			if(arr[i]>arr[j]) {
+				
+				temp=arr[i];
+				
+				arr[i]=arr[j];
+				
+				arr[j]=temp;
+			}
+		}
+		
+			System.out.println(arr[i]);
+		
+		
+	}
+}
+}
